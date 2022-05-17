1142ae1d9e1db39a2e7f583cf1839d389bf82001
Davide Merli <davide.merli@outlook.com>
2022-05-18 00:21:40 +0200

    feat: more comment styling, clickable links and selectable text

--- app/src/main/java/it/devddk/hackernewsclient/pages/SingleNewsPage.kt
+++ app/src/main/java/it/devddk/hackernewsclient/pages/SingleNewsPage.kt
@@ -7,16 +7,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
@@ -26,6 +30,7 @@ import androidx.core.text.HtmlCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import it.devddk.hackernewsclient.R
+import it.devddk.hackernewsclient.components.LinkifyText
 import it.devddk.hackernewsclient.domain.model.items.Item
 import it.devddk.hackernewsclient.domain.model.utils.ItemId
 import it.devddk.hackernewsclient.utils.TimeDisplayUtils
@@ -35,6 +40,7 @@ import it.devddk.hackernewsclient.viewmodels.SingleNewsViewModel
 
 
 @Composable
+@ExperimentalMaterial3Api
 fun SingleNewsPage(navController: NavController, id: Int?) {
 
     val mViewModel: SingleNewsViewModel = viewModel()
@@ -51,16 +57,13 @@ fun SingleNewsPage(navController: NavController, id: Int?) {
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
+@ExperimentalMaterial3Api
 fun Comments(item: Item) {
 
     val mViewModel: SingleNewsViewModel = viewModel()
     val scrollState = rememberLazyListState()
     val comments = mViewModel.commentsMap.collectAsState()
-
-    val expandedItems = remember { mutableSetOf<ItemId>() }
 
     Scaffold(topBar = {
         CenterAlignedTopAppBar(
@@ -78,22 +81,24 @@ fun Comments(item: Item) {
             },
         )
     }, containerColor = MaterialTheme.colorScheme.background) {
-        LazyColumn(
-            state = scrollState,
-            modifier = Modifier.padding(top = it.calculateTopPadding())
-        ) {
+        LazyColumn(state = scrollState,
+            modifier = Modifier.padding(top = it.calculateTopPadding())) {
             item {
-                Text(
-                    "${item.title}",
-                    style = MaterialTheme.typography.bodyLarge.copy(
-                        color = MaterialTheme.colorScheme.onSurface,
+                Text("${Html.fromHtml(item.title, HtmlCompat.FROM_HTML_MODE_COMPACT)}",
+                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onSurface,
                         fontWeight = FontWeight.SemiBold,
-                        lineHeight = 19.5.sp
-                    )
-                )
+                        lineHeight = 19.5.sp))
                 // Don't display it if it is null
-                if (item.text != null) {
-                    Text("${item.text}")
+                item.text?.let { text ->
+                    SelectionContainer {
+                        LinkifyText("${Html.fromHtml(text, HtmlCompat.FROM_HTML_MODE_COMPACT)}",
+                            modifier = Modifier.padding(8.dp),
+                            style = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface,
+                                fontWeight = FontWeight.Light,
+                                lineHeight = 19.5.sp),
+                            linkColor = MaterialTheme.colorScheme.secondary)
+                    }
                 }
             }
 
@@ -105,8 +110,7 @@ fun Comments(item: Item) {
                     }
                 }
                 thisComment?.let { it ->
-                    val commentState =
-                        comments.value.getOrDefault(it, CommentUiState.Loading)
+                    val commentState = comments.value.getOrDefault(it, CommentUiState.Loading)
 
                     ExpandableComment(
                         commentState,
@@ -145,6 +149,7 @@ fun ExpandableComment(
                 LaunchedEffect(childId) {
                     mViewModel.getItem(childId)
                 }
+
                 ExpandableComment(comments.getOrDefault(childId, CommentUiState.Loading),
                     comments,
                     depth + 1)
@@ -155,97 +160,94 @@ fun ExpandableComment(
 
 @Composable
 fun CommentCard(commentState: CommentUiState, depth: Int, onClick: () -> Unit, expanded: Boolean) {
-    val depthColors: List<Color> = listOf(
-        Color(0xffef476f),
-        Color(0xffffd166),
-        Color(0xff06d6a0),
-        Color(0xff118ab2),
-        Color(0xff073b4c)
-    )
+    val depthColors: List<Color> = listOf(Color(0x66ef476f),
+        Color(0x66ffd166),
+        Color(0x6606d6a0),
+        Color(0x66118ab2),
+        Color(0x66073b4c))
 
     val context = LocalContext.current
 
-    Box(Modifier.clickable {
-        onClick()
-    }) {
-        Row(
-            modifier = Modifier
-                .height(IntrinsicSize.Min)
-                .padding(start = (depth * 6).dp, top = 4.dp, end = 4.dp, bottom = 4.dp)
-                .clip(RoundedCornerShape(4.dp))
-                .background(Color.DarkGray)
-        ) {
-            Box(
-                modifier = Modifier
-                    .width(10.dp)
-                    .fillMaxHeight()
-                    .padding(end = 4.dp)
-                    .clip(RoundedCornerShape(4.dp))
-                    .background(depthColors[depth % depthColors.size])
-            )
+    Row(modifier = Modifier
+        .height(IntrinsicSize.Min)
+        .padding(start = ((depth + 1) * 6).dp, top = 4.dp, end = 4.dp, bottom = 4.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colorScheme.secondary.copy(alpha = 0.1f))) {
+        Box(modifier = Modifier
+            .width(10.dp)
+            .fillMaxHeight()
+            .padding(end = 4.dp)
+            .clip(RoundedCornerShape(topStart = 4.dp, bottomStart = 4.dp))
+            .background(depthColors[depth % depthColors.size]))
 
-            when (commentState) {
-                is CommentUiState.CommentLoaded -> {
-                    val text = commentState.item.text?.let { text ->
-                        Html.fromHtml(text, HtmlCompat.FROM_HTML_OPTION_USE_CSS_COLORS)
-                    } ?: "< deleted comment >"
+        when (commentState) {
+            is CommentUiState.CommentLoaded -> {
+                val text = commentState.item.text?.let { text ->
+                    Html.fromHtml(text, HtmlCompat.FROM_HTML_OPTION_USE_CSS_COLORS)
+                } ?: "< deleted comment >"
 
-                    val timeString = remember(commentState.item) {
-                        TimeDisplayUtils(context).toDateTimeAgoInterval(commentState.item.time)
-                    }
+                val timeString = remember(commentState.item) {
+                    TimeDisplayUtils(context).toDateTimeAgoInterval(commentState.item.time)
+                }
 
-                    Column(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(
-                            "${commentState.item.by} - $timeString",
-                            modifier = Modifier.padding(4.dp),
-                            style = MaterialTheme.typography.bodyLarge.copy(
-                                fontWeight = FontWeight.SemiBold,
-                                textDecoration = TextDecoration.Underline,
-                                fontStyle = FontStyle.Italic
-                            ),
-                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = buildAnnotatedString {
+                            pushStyle(SpanStyle(textDecoration = TextDecoration.Underline,
+                                fontStyle = FontStyle.Italic,
+                                fontWeight = FontWeight.SemiBold))
+                            append(commentState.item.by ?: "")
+                            pop()
+                            append(" • ")
+                            append(timeString)
+                        },
+                        modifier = Modifier.padding(4.dp),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
 
-                            )
-
-                        Text(
+                    SelectionContainer {
+                        LinkifyText(
                             "${text.trim()}",
                             modifier = Modifier.padding(4.dp),
-                            style = MaterialTheme.typography.bodyMedium
+                            style = MaterialTheme.typography.bodyMedium,
+                            linkColor = MaterialTheme.colorScheme.secondary,
                         )
+                    }
 
-                        val numKids = commentState.item.kids.size
+                    val numKids = commentState.item.kids.size
 
-                        if (numKids > 0) {
-
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 8.dp),
-                                horizontalArrangement = Arrangement.Center
-                            ) {
-                                Text("" +
-                                        "$numKids comments",
-                                    style = MaterialTheme.typography.bodySmall
-                                )
-                                Icon(
-                                    if (expanded) Icons.Rounded.KeyboardArrowUp else Icons.Rounded.KeyboardArrowDown,
-                                    if (expanded) "Close Comments" else "Open Comments"
-                                )
+                    if (numKids > 0) {
+                        Box(modifier = Modifier
+                            .height(48.dp)
+                            .padding(vertical = 4.dp)
+                            .clickable {
+                                onClick()
+                            }) {
+                            Row(modifier = Modifier.fillMaxSize(),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically) {
+                                Text("$numKids comments",
+                                    style = MaterialTheme.typography.bodySmall)
+                                Icon(if (expanded) Icons.Rounded.KeyboardArrowUp else Icons.Rounded.KeyboardArrowDown,
+                                    if (expanded) "Close Comments" else "Open Comments")
                             }
                         }
                     }
-
                 }
-                is CommentUiState.Error -> Text("Errorrrrr",
-                    modifier = Modifier.padding(15.dp))
-                is CommentUiState.Loading -> Text("LOADING",
-                    modifier = Modifier.padding(15.dp))
+
             }
+            is CommentUiState.Error -> Text("< ERROR >",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth())
+
+            is CommentUiState.Loading -> Text("LOADING ...",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth())
         }
     }
-
 }
 
 
