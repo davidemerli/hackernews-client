8a6dd883520c2b1a24e2fb6d71686c35b9ea4954
Dario Passarello <dario.passarello@gmail.com>
2022-06-16 00:41:09 +0200

    feat: adding files for real

--- app/src/main/java/it/devddk/hackernewsclient/pages/FeedbackPage.kt
+++ app/src/main/java/it/devddk/hackernewsclient/pages/FeedbackPage.kt
@@ -1,5 +1,6 @@
 package it.devddk.hackernewsclient.pages
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -24,14 +25,20 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import it.devddk.hackernewsclient.components.HNModalNavigatorPanel
 import it.devddk.hackernewsclient.components.HomePageTopBar
 import it.devddk.hackernewsclient.components.SegmentedButtons
+import it.devddk.hackernewsclient.domain.model.feedback.Feedback
+import it.devddk.hackernewsclient.viewmodels.FeedbackViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
@@ -40,6 +47,9 @@ fun FeedbackPage(navController: NavController, itemId: Int? = null) {
 
     var feedbackMessage by remember { mutableStateOf("") }
     val uxValueState: MutableState<Int?> = remember { mutableStateOf(null) }
+    val viewModel: FeedbackViewModel = viewModel()
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     val scrollState = rememberScrollState()
 
@@ -61,7 +71,24 @@ fun FeedbackPage(navController: NavController, itemId: Int? = null) {
                 ExtendedFloatingActionButton(
                     text = { Text("Submit") },
                     icon = { Icon(Icons.Filled.Send, "Send Feedback") },
-                    onClick = { /*TODO*/ },
+                    onClick = {
+                        coroutineScope.launch {
+                            viewModel.postFeedback(Feedback(itemId,
+                                feedbackMessage,
+                                uxValueState.value ?: -1)).fold(
+                                onSuccess = {
+                                    Toast.makeText(context,
+                                        "Thanks for the feedback",
+                                        Toast.LENGTH_SHORT).show()
+                                    navController.popBackStack() },
+                                onFailure = {
+                                    Toast.makeText(context,
+                                        "Failed to send feedback",
+                                        Toast.LENGTH_SHORT).show()
+                                }
+                            )
+                        }
+                    },
                 )
             }
         ) {
