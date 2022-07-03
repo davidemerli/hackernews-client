2ef805887212e7af0ad9f4e64e6f801cf6b345cd
Davide Merli <davide.merli@outlook.com>
2022-07-03 16:47:20 +0200

    feat: performance improvents

--- app/src/main/java/it/devddk/hackernewsclient/pages/home/components/NewsColumn.kt
+++ app/src/main/java/it/devddk/hackernewsclient/pages/home/components/NewsColumn.kt
@@ -1,12 +1,16 @@
 package it.devddk.hackernewsclient.pages.home.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import it.devddk.hackernewsclient.domain.model.collection.UserDefinedItemCollection
 import it.devddk.hackernewsclient.domain.model.items.Item
@@ -16,6 +20,7 @@ import it.devddk.hackernewsclient.shared.components.news.SwipeableNewsItem
 import it.devddk.hackernewsclient.viewmodels.ItemCollectionHolder
 import it.devddk.hackernewsclient.viewmodels.NewsItemState
 import it.devddk.hackernewsclient.viewmodels.NewsPageState
+import kotlin.math.min
 
 @Composable
 fun ColumnScope.NewsColumn(
@@ -24,8 +29,19 @@ fun ColumnScope.NewsColumn(
     onItemClick: (Item) -> Unit,
     toggleCollection: (Item, UserDefinedItemCollection) -> Unit,
 ) {
-    val pageState by remember { itemCollection.pageState }.collectAsState(NewsPageState.Loading)
+    var hasLoaded by rememberSaveable { mutableStateOf(false) }
+
+    val pageState by remember { itemCollection.pageState }.collectAsState(if (hasLoaded) NewsPageState.NewsIdsLoaded(emptyList()) else NewsPageState.Loading)
     val itemListState by remember { itemCollection.itemListFlow }.collectAsState(initial = emptyList())
+
+    var initialApiCalled by rememberSaveable { mutableStateOf(false) }
+
+    if (!initialApiCalled && !hasLoaded) {
+        LaunchedEffect(Unit) {
+            itemCollection.loadAll()
+            initialApiCalled = true
+        }
+    }
 
     when (pageState) {
         is NewsPageState.Loading -> LoadingPage(modifier, itemCollection = itemCollection)
@@ -33,7 +49,11 @@ fun ColumnScope.NewsColumn(
         is NewsPageState.NewsIdsError -> LoadPageError()
 
         is NewsPageState.NewsIdsLoaded -> {
-            itemListState.subList(0, 10).forEach { itemState ->
+            hasLoaded = true
+
+            val length = min(10, itemListState.size)
+
+            itemListState.subList(0, length).forEach { itemState ->
                 when (itemState) {
                     is NewsItemState.Loading -> {
                         LaunchedEffect(itemState.itemId) {
@@ -64,6 +84,4 @@ internal fun LoadingPage(
     itemCollection: ItemCollectionHolder
 ) {
     LoadingIndicatorCircular(modifier = modifier.fillMaxSize())
-
-    LaunchedEffect(Unit) { itemCollection.loadAll() }
 }
